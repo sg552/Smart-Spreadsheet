0e81b7a398975c23500c3692d663c3ee6270c8c4
siwei <shensiwei@sina.com>
2024-06-09 13:09:51 +0800

    finished: get table top right

--- table_helper.py
+++ table_helper.py
@@ -25,6 +25,10 @@ class TableHelper:
         sheet = workbook.active
         return sheet
 
+    # 左边没有任何空白，
+    # 左边是2个空白，本身是 HEADER_COLOR_INDEX的，一定是table起点
+    # 蓝色可以向右有一个空白，然后继续蓝色
+    # 可以向下连续一行
     def is_table_top_left(self, sheet, i, j):
         rows = list(sheet.iter_rows())
         #print(f"== cell: {rows[i][j]}")
@@ -39,55 +43,40 @@ class TableHelper:
         else:
             return False
 
+    # 右边两个肯定都是空白, 可能一行存在多个。找到立刻return
+    def is_table_top_right(self, sheet, i, j):
+        rows = list(sheet.iter_rows())
+        row = rows[i]
+
+        return self.is_table_header(row[j]) and \
+                (not self.is_table_header(row[j + 1])) and \
+                (not self.is_table_header(row[j + 2]))
+
 
     # 获得该table top, left, right , bottom
     def get_single_table(self, sheet):
-        table = Table([[]])
 
+        # get all tables top left
         for i, row in enumerate(sheet.iter_rows()):
             for j, cell in enumerate(row):
                 if i > 100:
                     break
-
                 print(f"cell: {cell.value}, i: {i}, j: {j}, {row[j].value}")
-
-                # 左边没有任何空白，
-                # 左边是2个空白，本身是 HEADER_COLOR_INDEX的，一定是table起点
-                # 蓝色可以向右有一个空白，然后继续蓝色
-                # 可以向下连续一行
-
-                if (j == 0 and self.is_table_header(cell) ) or \
-                        (j == 1 and self.is_table_header(cell) and  row[j - 1].value == None)  or \
-                        (j == 2 and self.is_table_header(cell) and row[j - 2].value == None and row[j - 1].value == None):
-                    # 判断一下上一行的cell是否是header, 是的话，本身则不是header了。
-                    if i == 0:
-                        return True
-                    else:
-                        return not self.is_table_header(sheet.iter_rows()[i-1][j])
-
+                if(self.is_table_top_left(sheet, i, j)):
+                    table = Table([[]])
                     print(f"=== found table top left: column: {cell.column}, row: {cell.row}")
                     table.left = cell.column
                     table.top = cell.row
+                    self.tables.append(table)
 
-                # 判断table header( top left ): 根据颜色吧。
-                #if cell = ''
-                #    and cell_down != ''
-                #    and (not is_my_digit(cell_down))
-                #    and cell_left == ''
-                #    and (
-                #            is_my_string(cell_right)
-                #            or ( cell_right == '' and is_my_string(cell_right))
-                #            ):
-                #    table.left = j
-                #    table.top = i
+        # get all tables top right
+        #for table in self.tables:
+        #    target_row = list(sheet.iter_rows())[table.top - 1]
+        #    for i, cell in target_row:
+        #        self.is_table_top_right(target_row, i):
 
-                #else:
-                #    pass
 
-                #if cell == '' and cell_right == '' and cell_right_right == '':
-                #    table.right = j
 
-                #if cell == '' and
 
 
 
--- test_table_helper.py
+++ test_table_helper.py
@@ -9,15 +9,6 @@ class TestTableHelper(unittest.TestCase):
     def test_is_table_top_left(self):
         sheet = self.table_helper.open_file('tests/example_0.xlsx')
 
-        #=== found table top left: column: 3, row: 1
-        #=== found table top left: column: 3, row: 19
-        #=== found table top left: column: 3, row: 39
-        #=== found table top left: column: 3, row: 51
-        #=== found table top left: column: 3, row: 62
-        #=== found table top left: column: 3, row: 67
-        #=== found table top left: column: 3, row: 73
-        #=== found table top left: column: 3, row: 74
-        #=== found table top left: column: 3, row: 84
         self.assertEqual(True, self.table_helper.is_table_top_left(sheet, 1-1, 3-1))
         self.assertEqual(True, self.table_helper.is_table_top_left(sheet, 19-1, 3-1))
         self.assertEqual(True, self.table_helper.is_table_top_left(sheet, 39-1, 3-1))
@@ -28,10 +19,17 @@ class TestTableHelper(unittest.TestCase):
         self.assertEqual(False, self.table_helper.is_table_top_left(sheet, 74-1, 3-1))
         self.assertEqual(True, self.table_helper.is_table_top_left(sheet, 84-1, 3-1))
 
-    def test_get_single_table(self):
+    def test_is_table_top_right(self):
 
         sheet = self.table_helper.open_file('tests/example_0.xlsx')
-        table = self.table_helper.get_single_table(sheet)
+        self.assertEqual(False, self.table_helper.is_table_top_right(sheet, 1-1, 6 ))
+        self.assertEqual(True, self.table_helper.is_table_top_right(sheet, 1-1, 7 ))
+        self.assertEqual(False, self.table_helper.is_table_top_right(sheet, 1-1, 8 ))
+
+    #def test_get_single_table(self):
+
+    #    sheet = self.table_helper.open_file('tests/example_0.xlsx')
+    #    table = self.table_helper.get_single_table(sheet)
         #self.assertEqual(matrix1, table.data)
 
 if __name__ == '__main__':
